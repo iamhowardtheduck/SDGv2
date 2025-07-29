e01cf88a2938b0f2350a57365fa0587908ba2fb6
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-07-29 14:21:16 -0400

    Update setup-SAIFE.sh

--- Scripts/setup-SAIFE.sh
+++ Scripts/setup-SAIFE.sh
@@ -79,7 +79,7 @@ echo
 
 # Load index templates for enrichment data
 curl -X POST "http://localhost:30920/_index_template/enrich-bluecoat" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/Enrichment-Index-Templates/enrich-bluecoat.json
-curl -X POST "http://localhost:30920/_index_template/enrich-user_agents" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/Enrichment-Index-Templates/enrich-nginx.json
+curl -X POST "http://localhost:30920/_index_template/enrich-nginx" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/Enrichment-Index-Templates/enrich-nginx.json
 curl -X POST "http://localhost:30920/_index_template/enrich-rip" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/Enrichment-Index-Templates/enrich-rip.json
 curl -X POST "http://localhost:30920/_index_template/enrich-user_agents" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/Enrichment-Index-Templates/enrich-user_agents.json
 curl -X POST "http://localhost:30920/_index_template/enrich-windows.sysmon_operational" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/Enrichment-Index-Templates/enrich-windows.sysmon_operational.json
