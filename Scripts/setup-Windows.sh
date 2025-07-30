bbded370ed35dc98f1b9d40461bc5edce55e2ba6
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-07-30 12:30:36 -0500

    Update setup-Windows.sh

--- Scripts/setup-Windows.sh
+++ Scripts/setup-Windows.sh
@@ -1,3 +1,12 @@
+# DELETE Pre-Configured Windows templates
+curl -X DELETE "http://localhost:30920/_component_template/logs-windows.sysmon_operational@package" -H "Content-Type: application/json" -u "sdg:changeme"
+curl -X DELETE "http://localhost:30920/_index_template/logs-windows.sysmon_operational" -H "Content-Type: application/json" -u "sdg:changeme"
+
+
+# Recreate them in your image
 curl -X PUT "http://localhost:30920/_component_template/logs-windows.sysmon_operational@package" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Component-Templates/logs-windows.sysmon_operational.json
+curl -X PUT "http://localhost:30920/_index_template/logs-windows.sysmon_operational" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/logs-windows.sysmon_operational.json
+
+
 
 java -jar /root/SDGv2/build/libs/SDGv2-1.0.0-SNAPSHOT.jar /root/SDGv2/Tracks/saife-windows.yml
