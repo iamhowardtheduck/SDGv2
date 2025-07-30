1f9288b941149fcaa0017acf5b049d5993319807
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-07-30 11:56:01 -0500

    Update setup-Windows.sh

--- Scripts/setup-Windows.sh
+++ Scripts/setup-Windows.sh
@@ -1,1 +1,3 @@
 curl -X PUT "http://localhost:30920/_component_template/logs-windows.sysmon_operational@package" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Component-Templates/logs-windows.sysmon_operational.json
+
+java -jar /root/SDGv2/build/libs/SDGv2-1.0.0-SNAPSHOT.jar /root/SDGv2/Tracks/saife-windows.yml
