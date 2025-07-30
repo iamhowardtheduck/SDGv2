c5139ff93fa26545d3f7341422a58c52fca4eb2d
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-07-30 12:55:55 -0500

    Update setup-Windows.sh

--- Scripts/setup-Windows.sh
+++ Scripts/setup-Windows.sh
@@ -2,11 +2,11 @@
 curl -X DELETE "http://localhost:30920/_index_template/logs-windows.sysmon_operational" -H "Content-Type: application/json" -u "sdg:changeme"
 curl -X DELETE "http://localhost:30920/_component_template/logs-windows.sysmon_operational@package" -H "Content-Type: application/json" -u "sdg:changeme"
 
-
 # Recreate them in your image
 curl -X PUT "http://localhost:30920/_component_template/logs-windows.sysmon_operational@package" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Component-Templates/logs-windows.sysmon-operational.json
 curl -X PUT "http://localhost:30920/_index_template/logs-windows.sysmon_operational" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/logs-windows.sysmon_operational.json
 
-
+# Rollover existing index in order to inherit new settings without any garbage data
+curl -X POST "http://localhost:30920/logs-windows.sysmon_operation-default/_rollover" -u "sdg:changeme"
 
 java -jar /root/SDGv2/build/libs/SDGv2-1.0.0-SNAPSHOT.jar /root/SDGv2/Tracks/saife-windows.yml
