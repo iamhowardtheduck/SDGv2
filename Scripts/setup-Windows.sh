cd6b445ac03150afd7e8f75b063774c15488358e
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-07-30 15:11:23 -0500

    Update setup-Windows.sh

--- Scripts/setup-Windows.sh
+++ Scripts/setup-Windows.sh
@@ -1,14 +1,18 @@
 # DELETE Pre-Configured Windows templates
-#curl -X DELETE "http://localhost:30920/_index_template/logs-windows.sysmon_operational" -H "Content-Type: application/json" -u "sdg:changeme"
-#curl -X DELETE "http://localhost:30920/_component_template/logs-windows.sysmon_operational@package" -H "Content-Type: application/json" -u "sdg:changeme"
+curl -X DELETE "http://localhost:30920/_index_template/logs-windows.sysmon_operational" -H "Content-Type: application/json" -u "sdg:changeme"
+curl -X DELETE "http://localhost:30920/_component_template/logs-windows.sysmon_operational@package" -H "Content-Type: application/json" -u "sdg:changeme"
 
-curl -X DELETE "http://localhost:30920//_data_stream/logs-windows.sysmon_operational-default" -u "sdg:changeme"
+# DELETE associated datastream if present
+#curl -X DELETE "http://localhost:30920//_data_stream/logs-windows.sysmon_operational-default" -u "sdg:changeme"
+
+# Create an updated ingest pipeline for the custom logs
+curl -X PUT "http://localhost:30920/_ingest/pipeline/logs@custom" -H "Content-Type: application/x-ndjson" -u "sdg:changeme" -d @/root/SDGv2/Ingest-Pipelines/logs@custom.json
 
 # Recreate them in your image
 curl -X PUT "http://localhost:30920/_component_template/logs-windows.sysmon_operational@package" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Component-Templates/logs-windows.sysmon-operational.json
 curl -X PUT "http://localhost:30920/_index_template/logs-windows.sysmon_operational" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/logs-windows.sysmon_operational.json
 
 # Rollover existing index in order to inherit new settings without any garbage data
-curl -X POST "http://localhost:30920/logs-windows.sysmon_operation-default/_rollover" -u "sdg:changeme"
+#curl -X POST "http://localhost:30920/logs-windows.sysmon_operation-default/_rollover" -u "sdg:changeme"
 
 java -jar /root/SDGv2/build/libs/SDGv2-1.0.0-SNAPSHOT.jar /root/SDGv2/Tracks/saife-windows.yml
