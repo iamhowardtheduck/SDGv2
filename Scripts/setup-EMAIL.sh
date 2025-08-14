5b7e8d5f7b489ad8e239b7475f2c2d88ce678148
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-08-14 11:18:14 -0400

    Create setup-EMAIL.sh

---
+++ Scripts/setup-EMAIL.sh
@@ -0,0 +1,12 @@
+# DELETE Pre-Configured logs-ti_abusech.malware templates
+curl -X DELETE "http://localhost:30920/_index_template/logs-ti_abusech.malware" -H "Content-Type: application/json" -u "sdg:changeme"
+curl -X DELETE "http://localhost:30920/_component_template/logs-ti_abusech.malware@package" -H "Content-Type: application/json" -u "sdg:changeme"
+
+# Recreate them in your image
+curl -X PUT "http://localhost:30920/_component_template/logs-ti_abusech.malware@package" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Component-Templates/logs-ti_abusech.malware.json
+curl -X PUT "http://localhost:30920/_index_template/logs-ti_abusech.malware" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/logs-ti_abusech.malware.json
+
+# Rollover existing index in order to inherit new settings without any garbage data
+#curl -X POST "http://localhost:30920/logs-windows.sysmon_operation-default/_rollover" -u "sdg:changeme"
+
+java -jar /root/SDGv2/build/libs/SDGv2-1.0.0-SNAPSHOT.jar /root/SDGv2/Tracks/saife-email.yml
