69f25df64ae2360f1b220f8aacfc7a5aac3d9513
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-08-14 15:26:51 -0400

    Update setup-EMAIL.sh

--- Scripts/setup-EMAIL.sh
+++ Scripts/setup-EMAIL.sh
@@ -7,6 +7,8 @@ curl -X DELETE "http://localhost:30920/_component_template/logs-ti_abusech.malwa
 # Recreate them in your image
 curl -X PUT "http://localhost:30920/_component_template/logs-ti_abusech.malware@package" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Component-Templates/logs-ti_abusech.malware.json
 curl -X PUT "http://localhost:30920/_index_template/logs-ti_abusech.malware" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/logs-ti_abusech.malware.json
+curl -X PUT "http://localhost:30920/_index_template/logs-email.filter" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/logs-email.filter.json
+
 
 # Rollover existing index in order to inherit new settings without any garbage data
 #curl -X POST "http://localhost:30920/logs-windows.sysmon_operation-default/_rollover" -u "sdg:changeme"
