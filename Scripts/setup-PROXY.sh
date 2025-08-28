674d5d809e7d069bd356cc527b2c7a29207bbe23
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-08-28 13:53:05 -0400

    Update setup-PROXY.sh

--- Scripts/setup-PROXY.sh
+++ Scripts/setup-PROXY.sh
@@ -1,5 +1,10 @@
-# Create logs-proxy templates
-curl -X PUT "http://localhost:30920/_index_template/logs-bluecoat.proxy" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/logs-bluecoat.proxy.json
+# DELETE Pre-Configured logs-proxysg.log templates
+curl -X DELETE "http://localhost:30920/_index_template/logs-ti_abusech.malwarebazaar" -H "Content-Type: application/json" -u "sdg:changeme"
+curl -X DELETE "http://localhost:30920/_component_template/logs-ti_abusech.malware@package" -H "Content-Type: application/json" -u "sdg:changeme"
+
+# Recreate them in your image
+curl -X PUT "http://localhost:30920/_component_template/logs-proxysg.log@package" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Component-Templates/logs-proxysg.log.json
+curl -X PUT "http://localhost:30920/_index_template/logs-proxysg.log" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/logs-proxysg.log.json
 
 # Begin data generation
 java -jar /root/SDGv2/build/libs/SDGv2-1.0.0-SNAPSHOT.jar /root/SDGv2/Tracks/saife-proxy.yml
