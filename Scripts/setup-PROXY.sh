9ec616385f8f47a637535732eab4d3b4e445de00
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-08-28 13:53:49 -0400

    Update setup-PROXY.sh

--- Scripts/setup-PROXY.sh
+++ Scripts/setup-PROXY.sh
@@ -1,6 +1,6 @@
 # DELETE Pre-Configured logs-proxysg.log templates
-curl -X DELETE "http://localhost:30920/_index_template/logs-ti_abusech.malwarebazaar" -H "Content-Type: application/json" -u "sdg:changeme"
-curl -X DELETE "http://localhost:30920/_component_template/logs-ti_abusech.malware@package" -H "Content-Type: application/json" -u "sdg:changeme"
+curl -X DELETE "http://localhost:30920/_index_template/logs-proxysg.log" -H "Content-Type: application/json" -u "sdg:changeme"
+curl -X DELETE "http://localhost:30920/_component_template/logs-proxysg.log@package" -H "Content-Type: application/json" -u "sdg:changeme"
 
 # Recreate them in your image
 curl -X PUT "http://localhost:30920/_component_template/logs-proxysg.log@package" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Component-Templates/logs-proxysg.log.json
