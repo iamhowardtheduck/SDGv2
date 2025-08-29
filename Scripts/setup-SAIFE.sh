3539d78de9cd6ae7487c623956360bc27b368a07
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-08-29 14:22:16 -0400

    Update setup-SAIFE.sh

--- Scripts/setup-SAIFE.sh
+++ Scripts/setup-SAIFE.sh
@@ -49,7 +49,7 @@ echo "Simple Data Generator installed"
 echo
 
 # Install LLM Connector
-bash /opt/workshops/elastic-llm.sh -k false -m anthropic -k true -d true
+bash /opt/workshops/elastic-llm.sh -k false -m anthropic -d true
 
 echo
 echo "AWS Bedrock AI Assistant Connector configured as OpenAI"
