7b85bb37352cb057a8eceaa828c4b8eebabf3497
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-09-05 21:48:40 -0400

    Update setup_School_Day_Lesson3.sh

--- Scripts/setup_School_Day_Lesson3.sh
+++ Scripts/setup_School_Day_Lesson3.sh
@@ -1,2 +1,2 @@
 # Begin data generation
-java -jar /root/SDGv2/build/libs/SDGv2-1.0.0-SNAPSHOT.jar /root/SDGv2/Tracks/schoolday_all.yml
+java -jar /root/SDGv2/build/libs/SDGv2-1.0.0-SNAPSHOT.jar /root/SDGv2/Tracks/schoolday_all_lesson3.yml
