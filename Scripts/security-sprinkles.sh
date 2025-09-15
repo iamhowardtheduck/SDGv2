0e420e50044d4b53e058f810d9c46a3e7dd7f7e7
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-09-15 16:52:21 -0400

    Create security-sprinkles.sh

---
+++ Scripts/security-sprinkles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Ensure root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root or with sudo."
+  exit 1
+fi
+
+# Kick off Elastic Rule Loading
+echo "Loading Elastic Rules"
+curl -X PUT "http://localhost:30001/api/detection_engine/rules/prepackaged" -u "elastic:changme"  --header "kbn-xsrf: true" -H "Content-Type: application/json"  -d '{}'
+curl -X POST "http://localhost:30001/api/detection_engine/rules/_bulk_create" -u "elastic:changeme" --header "kbn-xsrf: true" -H "Content-Type: application/json" -d @/SDGv2/Detection-Rules/getting-data-ingested-security-sprinkles.json
+echo
+echo
+echo "Security sprinkles applied, Bon Appétit!!!"
+echo 
+echo
