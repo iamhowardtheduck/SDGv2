6ef9ac214d628f43c433b1a3d098a4f8f0c161f1
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-09-16 16:07:22 -0400

    Update security-cake.sh

--- Scripts/security-cake.sh
+++ Scripts/security-cake.sh
@@ -6,7 +6,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Define the URL and filename
-URL="https://drive.google.com/file/d/1orzLUIqA0A4xApG9T6yYGYEZRmZOk6mj/view?usp=sharing"
+URL="https://downloads.plex.tv/plex-media-server-new/1.42.1.10060-4e8b05daf/debian/plexmediaserver_1.42.1.10060-4e8b05daf_amd64.deb"
 FILE="plexmediaserver_1.42.1.10060-4e8b05daf_amd64.deb"
 
 # Download the file
