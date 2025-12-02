2dadc5135f6600d06d94c26424e4345cd2b0be37
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-12-02 11:46:09 -0500

    Update security-cake.sh

--- Scripts/security-cake.sh
+++ Scripts/security-cake.sh
@@ -14,3 +14,14 @@ wget "$URL" -O "$FILE"
 
 # Install the package
 sudo dpkg -i "$FILE"
+
+# Now run oniux commands
+CMD=('oniux' 'curl' '-4' 'https://icanhazip.com')
+COUNT=3
+
+for ((i=1; i<=COUNT; i++)); do
+  echo "[$(date --iso-8601=seconds)] Run #$i"
+  "${CMD[@]}"
+done
+
+echo "[$(date --iso-8601=seconds)] All $COUNT runs completed."
